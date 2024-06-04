8032886ba7cb1fae33a7de81da18782241ae609d
leaaah2 <rissel.lea@gmail.com>
2024-06-04 22:14:21 +0200

    joueur

---
+++ Schotten_Totten/Schotten_Totten/Joueur.h
@@ -0,0 +1,10 @@
+#ifndef JOUEUR_HEADER
+#define JOUEUR_HEADER
+class Joueur {
+private:
+	int nbreCartes;
+	int nbreCartesTactiquesJoués;
+public:
+	int getNbreCartes() { return nbreCartes; };
+	int get nbreCartesTactiquesJoués() { return nbreCartesTactiquesJoués; };
+#endif
