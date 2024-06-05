5bc3d1a394dc608d6780a5ba5f761b85d2a16380
leaaah2 <rissel.lea@gmail.com>
2024-06-05 09:27:39 +0200

    borne commentaires

--- Schotten_Totten/Schotten_Totten/Borne.h
+++ Schotten_Totten/Schotten_Totten/Borne.h
@@ -10,15 +10,42 @@ private:
 	vector<unique_ptr<CarteClan>> cartesJ1;
 	vector<unique_ptr<CarteClan>> cartesJ2;
 public:
+	/**
+	* brief retourne le numero
+	*/
 	int getNumero(){ return numero; };
+	/**
+	* brief remplace le numero par un nouveau
+	*/
 	void setNumero(int nouvNum) { numero = nouvNum; };
+	/**
+	* brief retourne le statut de la borne
+	*/
 	int getSatut() { return statut; };
+	/**
+	* brief remplace le statut par un nouveay
+	*/
 	void setStatut(int nouvStat) { statut = nouvStat; };
+	/**
+	* brief retourne le le nombre de cartes pour revendiquer la borne
+	*/
 	unsigned int getNbCartesRevendiquer() { return nbCartesRevendiquer;};
+	/**
+	* brief retourne les cartes de J1 sur la borne
+	*/
 	vector<CarteClan&> getCartesJ1();
+	/**
+	* brief retourne les cartes de J2 sur la borne
+	*/
 	vector<CarteClan&> getCartesJ2();
-	void AjouterCartesJ1(CarteClan carteJ1);
-	void AjouterCartesJ2(CarteClan carteJ2);
+	/**
+	* brief Ajoute une carte de J1
+	*/
+	void AjouterCarteJ1(CarteClan carteJ1);
+	/**
+	* brief Ajoute une carte de J2
+	*/
+	void AjouterCarteJ2(CarteClan carteJ2);
 
 };
 #endif
