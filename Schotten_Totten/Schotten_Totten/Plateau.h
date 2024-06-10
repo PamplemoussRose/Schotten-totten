c30c25df6ecc8dcf7998bf0c38ba51484b8479ad
MissAzazel <rouxel.clara0@gmail.com>
2024-06-10 09:33:43 +0200

    commentaire
    plateau commentaires

--- Schotten_Totten/Schotten_Totten/Plateau.h
+++ Schotten_Totten/Schotten_Totten/Plateau.h
@@ -10,42 +10,29 @@ private:
 	vector<Pioche*> vPioche;
 public:
 	//Methodes
-	/********************************************************
-	* setBornes
-	* Entree : vector des bornes
-	* Sorties : void
-	* Entraine : defini les bornes du plateau
-	*********************************************************/
+	/*!
+	* \brief defini les bornes du plateau
+	*/
 	void setBornes(vector<Borne*> &&lstBornes);
-	/********************************************************
-	* getBornes
-	* Sorties : retourne le vecteur borne
-	*********************************************************/
+	/*!
+	* \brief retourne un pointeur vers les bornes
+	*/
 	vector<Borne*> getBornes();
-	/********************************************************
-	* getBornePosition
-	* Entree : int position
-	* Sorties : l'objet borne present a une position donnee
-	*********************************************************/
+	/*!
+	* \brief retourne un pointeur vers une borne d'une position donee
+	*/
 	Borne* getBornePosition(int position);
-	
-	/********************************************************
-	* setPioche
-	* Entree : vector de la pioche
-	* Sorties : void
-	* Entraine : defini la pioche du plateau
-	*********************************************************/
+	/*!
+	* \brief defini la pioche du plateau
+	*/
 	void setPioche(vector<Pioche*>&& lstPioche);
-	/********************************************************
-	* getPioche
-	* Sorties : retourne le vecteur pioche
-	*********************************************************/
+	/*!
+	* \brief retourne un pointeur vers la pioche
+	*/
 	vector<Pioche*> getPioche();
-	/********************************************************
-	* getPiochePosition
-	* Entree : int position
-	* Sorties : l'objet pioche present a une position donnee
-	*********************************************************/
+	/*!
+	* \brief retourne un pointeur vers la pioche a une position donnee
+	*/
 	Pioche getPiochePosition(int position);
 };
 #endif
