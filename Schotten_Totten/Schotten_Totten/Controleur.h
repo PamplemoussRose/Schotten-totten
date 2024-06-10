37b993ab987edb162bcc3a23d496e1837e358e7f
Léo T <leo.thomas2003@orange.fr>
2024-06-10 08:29:52 +0200

    Ajout vue controleur

--- Schotten_Totten/Schotten_Totten/Controleur.h
+++ Schotten_Totten/Schotten_Totten/Controleur.h
@@ -1,11 +1,22 @@
 #ifndef GUARD_CONTROLEUR
 #define GUARD_CONTROLEUR
+#include "Vue.h"
 
 //Classe abstraite
 class Controleur {
+protected:
+	Vue* vue;
 
 	virtual void afficheVue() = 0;
 	
+public:
+	Controleur(Vue& pVue) {
+		vue = &pVue;
+	}
+
+	virtual void afficheVue() {
+		vue->affiche();
+	};
 };
 
 #endif // !GUARD_CONTROLEUR
