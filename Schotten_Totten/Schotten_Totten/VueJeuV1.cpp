bfc26dcf0773b82857cd2476d477f798d42b1c60
MissAzazel <rouxel.clara0@gmail.com>
2024-06-19 23:43:13 +0200

    VueJeuV1.cpp

--- Schotten_Totten/Schotten_Totten/VueJeuV1.cpp
+++ Schotten_Totten/Schotten_Totten/VueJeuV1.cpp
@@ -1,25 +1,32 @@
-#include"VueJeuV1.h"
+#include "VueJeuV1.h"
 #include "ControleurJeuV1.h"
 
 void VueJeuV1::afficheTour(Joueur& joueurActuel)
 {
+    affichePlateau();
+    unsigned int indiceCarte;
+    unsigned int numBorne;
+    cout << "Quelle carte voulez vous jouer? (1-6)" << endl;
+    afficheMain(joueurActuel);
+    cin >> indiceCarte;
 
-	affichePlateau();
-	unsigned int indiceCarte;
-	unsigned int numBorne;
-	cout << "Quelle carte voulez vous jouer? (1-6)" << endl;
-	afficheMain(joueurActuel);
-	cin >> indiceCarte;
-	CarteClan* carteChoisie;// = DemandeCarte::indiceCarte-1
-	cout << "Sur quel borne? (num�ro::1-9)" << endl;
-	cin >> numBorne;
-	Plateau* plateau = controleurV1->getEtatJeu()->getPlateau();
-	vector<Borne*> bornes = plateau->getBornes();
-	controleurV1->jouerCarteSurBorne(*carteChoisie,*bornes[numBorne-1]); 
-	controleurV1->revendiqueBorne(numBorne); //revendication de la borne ou pas
-	Carte* cartePiochee = controleurV1->getControPioche()->piocher(*plateau->getPiocheClan(),joueurActuel); //le joueur pioche
-	cout << "Vous avez pioche";
-	afficheCarte(cartePiochee);
-	cout << "Fin de votre tour" << endl;//avec appli ou etatjeu changement de joueur
+    // Obtenir la carte choisie
+    Carte* carteChoisie = joueurActuel.getCarteMainPosition(indiceCarte - 1);
 
+    // Vérifier que la carte est bien de type CarteClan
+    CarteClan* carteClanChoisie = dynamic_cast<CarteClan*>(carteChoisie);
+
+    cout << "Sur quelle borne? (numéro: 1-9)" << endl;
+    cin >> numBorne;
+
+    Plateau* plateau = controleurV1->getEtatJeu()->getPlateau();
+    vector<Borne*> bornes = plateau->getBornes();
+
+    controleurV1->jouerCarteSurBorne(*carteClanChoisie, *bornes[numBorne - 1]);
+    controleurV1->revendiqueBorne(numBorne);
+
+    Carte* cartePiochee = controleurV1->getControPioche()->piocher(*plateau->getPiocheClan(), joueurActuel);
+    cout << "Vous avez pioché : ";
+    afficheCarte(cartePiochee);
+    cout << "Fin de votre tour" << endl;
 }
